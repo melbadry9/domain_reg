8ea00b1e473cc05e1bdbdd682ead74727992a991
Mohamed Elbadry <me@melbadry9.xyz>
2021-03-19 19:50:52 +0200

    fix io::err

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,11 @@ fn main() {
 }
 
 fn check_av(domain: &String) {
-    let res = requests::get(format!("https://ae.godaddy.com/domainfind/v1/search/exact?key=dpp_search&partialQuery={}&q={}&req_id=1616029383106&solution_set_ids=dpp-us-solution-tier1%2Cdpp-intl-solution-tier4%2Cdpp-intl-solution-tier6%2Co365-solutionset-tier3%2Cdpp-us-solution-fixed-tier4&itc=dpp_absol1", &domain, &domain)).unwrap();
-    let data = res.json().unwrap();
-    println!("{}: {}", &domain, &data["ExactMatchDomain"]["AvailabilityLabel"]);
+    let url = format!("https://ae.godaddy.com/domainfind/v1/search/exact?key=dpp_search&partialQuery={}&q={}&req_id=1616029383106&solution_set_ids=dpp-us-solution-tier1%2Cdpp-intl-solution-tier4%2Cdpp-intl-solution-tier6%2Co365-solutionset-tier3%2Cdpp-us-solution-fixed-tier4&itc=dpp_absol1", &domain, &domain);
+    let res = requests::get(url);
+
+    match res  {
+        Ok(res) =>  println!("{}: {}", &domain, res.json().unwrap()["ExactMatchDomain"]["AvailabilityLabel"]),
+        Err(_err) => println!("{}: ERROR", &domain)
+    };
 }
